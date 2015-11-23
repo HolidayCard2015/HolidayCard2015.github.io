3e5815d76486ceacff9ff2d3ba87f6a289fdefdb
Daniel Susetyo <daniel.susetyo@gmail.com>
2015-11-22 23:02:42 -0800

    scroll to top, nav scroll to

--- js/main.js
+++ js/main.js
@@ -22,4 +22,23 @@ $(function() {
     $('#cta .cta-button').removeClass('hide');
   });
 
+// back to top
+  $('.icon-backtotop').on('click', function(){
+    $('html, body').animate({ scrollTop: 0 }, 'fast');
+  });
+
+// navigation
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top - 100
+            }, 1000);
+            return false;
+        }
+      }
+  });
+
 });
